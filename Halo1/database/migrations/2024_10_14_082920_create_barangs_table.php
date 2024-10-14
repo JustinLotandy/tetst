35a2c1206c842ca464147dc56dbb712b53d56e25
Justin Lotandy <vayss100@gmail.com>
2024-10-14 16:05:01 +0700

    s

--- Halo1/database/migrations/2024_10_14_082920_create_barangs_table.php
+++ Halo1/database/migrations/2024_10_14_082920_create_barangs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMatakuliahsTable extends Migration
+class Createbarangstable extends Migration
 {
     /**
      * Run the migrations.
